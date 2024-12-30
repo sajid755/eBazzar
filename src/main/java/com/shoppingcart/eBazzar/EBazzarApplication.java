a634369db5b05166ae4fa8483d887cdc0d763137
Sajidul <sajidul.saif.71@gmail.com>
2024-12-30 17:37:19 +0600

    test commit

---
+++ src/main/java/com/shoppingcart/eBazzar/EBazzarApplication.java
@@ -0,0 +1,13 @@
+package com.shoppingcart.eBazzar;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EBazzarApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EBazzarApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/shoppingcart/eBazzar/EBazzarApplicationTests.java
@@ -0,0 +1,13 @@
+package com.shoppingcart.eBazzar;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EBazzarApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
