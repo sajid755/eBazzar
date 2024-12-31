c480e15003b5827c8d11bcafce2b4ecb8c19a2a8
Sajidul <sajidul.saif.71@gmail.com>
2024-12-31 23:52:13 +0600

    Product Model Created

--- src/main/java/com/shoppingcart/eBazzar/model/Category.java
+++ src/main/java/com/shoppingcart/eBazzar/model/Category.java
@@ -1,25 +1,3 @@
-//package com.shoppingcart.eBazzar.model;
-//
-//import jakarta.persistence.OneToMany;
-//import lombok.AllArgsConstructor;
-//import lombok.Getter;
-//import lombok.NoArgsConstructor;
-//import lombok.Setter;
-//import org.springframework.web.bind.annotation.GetMapping;
-//
-//import java.util.*;
-//@Getter
-//@Setter
-//@NoArgsConstructor
-//@AllArgsConstructor
-//public class Category {
-//    private Long id;
-//    private String name;
-//
-//    @OneToMany(mappedBy = "category")
-//    private List<Product> produccts;
-//
-//}
 package com.shoppingcart.eBazzar.model;
 
 import jakarta.persistence.*;
