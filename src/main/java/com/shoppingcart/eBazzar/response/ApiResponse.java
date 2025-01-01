24bf961eea8ab48c3a5c8f02be56104108ea24d5
Sajidul <sajidul.saif.71@gmail.com>
2025-01-01 23:46:13 +0600

    Respose Model Created

---
+++ src/main/java/com/shoppingcart/eBazzar/response/ApiResponse.java
@@ -0,0 +1,11 @@
+package com.shoppingcart.eBazzar.response;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+
+@AllArgsConstructor
+@Data
+public class ApiResponse {
+    private String message;
+    private Object data;
+}
