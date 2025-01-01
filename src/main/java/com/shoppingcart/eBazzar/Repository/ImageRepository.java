8a4efa8cfc6b94a77802cee1e4d7012ebfa0e364
Sajidul <sajidul.saif.71@gmail.com>
2025-01-01 23:01:10 +0600

    Image Repo Implemented for Jpa

---
+++ src/main/java/com/shoppingcart/eBazzar/Repository/ImageRepository.java
@@ -0,0 +1,8 @@
+package com.shoppingcart.eBazzar.Repository;
+
+import com.shoppingcart.eBazzar.model.Image;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface ImageRepository extends JpaRepository<Image, Long> {
+
+}
