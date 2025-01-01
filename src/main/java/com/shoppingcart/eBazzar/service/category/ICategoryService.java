6c8324add6e3ba6b573ecf75dfcfa8e9a6b693be
Sajidul <sajidul.saif.71@gmail.com>
2025-01-01 21:43:52 +0600

    Category Interface Created

---
+++ src/main/java/com/shoppingcart/eBazzar/service/category/ICategoryService.java
@@ -0,0 +1,15 @@
+package com.shoppingcart.eBazzar.service.category;
+
+import com.shoppingcart.eBazzar.model.Category;
+
+import java.util.List;
+
+public interface ICategoryService {
+    Category getCategoryById(Long id);
+    Category getCategoryByName(String name);
+    List<Category> getAllCategories();
+    Category addCategory(Category category);
+    Category updateCategory(Category category, Long id);
+    void deleteCategoryById(Long id);
+
+}
