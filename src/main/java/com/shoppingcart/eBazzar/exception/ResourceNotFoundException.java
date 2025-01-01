48cf6c17b999cf7052c1ba9b7f02bfbfae110e6d
Sajidul <sajidul.saif.71@gmail.com>
2025-01-01 21:34:05 +0600

    ResourceNotFoundException implemented

---
+++ src/main/java/com/shoppingcart/eBazzar/exception/ResourceNotFoundException.java
@@ -0,0 +1,8 @@
+package com.shoppingcart.eBazzar.exception;
+
+public class ResourceNotFoundException extends RuntimeException {
+    public ResourceNotFoundException(String message) {
+        super(message);
+
+    }
+}
