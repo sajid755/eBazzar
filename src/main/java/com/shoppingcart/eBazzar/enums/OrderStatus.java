9d9a695db110eb9bf58b38ddfb15a810736f8e8b
Sajidul <sajidul.saif.71@gmail.com>
2025-01-04 21:48:57 +0600

    OrderItem, Order, OrderStatus Models Created

---
+++ src/main/java/com/shoppingcart/eBazzar/enums/OrderStatus.java
@@ -0,0 +1,9 @@
+package com.shoppingcart.eBazzar.enums;
+
+public enum OrderStatus {
+    PENDING,
+    PROCESSING,
+    SHIPPED,
+    DELIVERED,
+    CANCELED
+}
---
+++ src/main/java/com/shoppingcart/eBazzar/model/Order.java
@@ -0,0 +1,35 @@
+package com.shoppingcart.eBazzar.model;
+
+import java.math.BigDecimal;
+import java.time.LocalDate;
+import java.util.Set;
+
+import com.shoppingcart.eBazzar.enums.OrderStatus;
+
+import jakarta.persistence.CascadeType;
+import jakarta.persistence.Entity;
+import jakarta.persistence.EnumType;
+import jakarta.persistence.Enumerated;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.OneToMany;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@Getter
+@Setter
+@NoArgsConstructor
+@Entity
+public class Order {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long orderId;
+    private LocalDate orderDate;
+    private BigDecimal totalAmount;
+    @Enumerated(EnumType.STRING)
+    private OrderStatus orderStatus;
+    @OneToMany(mappedBy = "order", cascade = CascadeType.ALL, orphanRemoval = true)
+    private Set<OrderItem> orderItems;
+}
